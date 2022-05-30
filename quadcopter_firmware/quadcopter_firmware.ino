6a708e5d3edb93956bf490be5d1196cc8d9b8166
Justin Kleiber <jkleiber8@gmail.com>
2022-05-30 13:41:23 -0400

    Updates the comments for the quadrotor firmware

--- quadcopter_firmware/quadcopter_firmware.ino
+++ quadcopter_firmware/quadcopter_firmware.ino
@@ -1,32 +1,56 @@
 
 #include <IBusBM.h>
 
+// IBus Interface
 IBusBM ibus;
-char buf[256];
+
+// Remote control values
+int channel_values[6];
+
+// Debugging
+bool is_debug = true;
+
+
 
 void setup() {
-  Serial.begin(115200);
+  // Set up USB Serial for debugging
+  if (is_debug)
+  {
+    Serial.begin(115200);
+  }
   
-  // put your setup code here, to run once:
+  // Set up the IBusBM Serial interface.
+  // Use IBUSBM_NOTIMER to run the loop ourselves.
+  // Timing in IBusBM is not supported on Teensy 3.6.
   ibus.begin(Serial4, IBUSBM_NOTIMER);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int val = 0;
 
+  // Run the IBusBM loop here since the interrupt 
+  // timing is not supported on Teensy 3.6.
   ibus.loop();
 
+  // Read remote control channels
   for(int i = 0; i < 6; i++)
   {
-    val = ibus.readChannel(i);
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.print(val);
-    Serial.print(" ");
+    channel_values[i] = ibus.readChannel(i);
+
+    if (is_debug)
+    {
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.print(channel_values[i]);
+      Serial.print(" ");
+    }
   }
-  Serial.println();
 
+  // Print a new line for debugging.
+  if(is_debug)
+  {
+    Serial.println();
+  }
 
-  delay(20);
+  // Run approximately 200Hz
+  delay(5);
 }
