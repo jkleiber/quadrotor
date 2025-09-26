a99abd1ed014c1d4d50a765fe3d3e79f7cd3a239
Justin Kleiber <jkleiber8@gmail.com>
2025-09-26 03:04:52 -0400

    Base world for quadrotor sim in unreal engine 5

---
+++ QuadrotorSim/README.md
@@ -0,0 +1,8 @@
+
+# QuadrotorSim
+
+A simulator for the quadrotor to operate in a more real-world environment.
+
+## Dependencies
+UE5 Plugins:
+- [Cesium for Unreal v2.19.1](https://github.com/CesiumGS/cesium-unreal/releases/tag/v2.19.1)
---
+++ QuadrotorSim/Source/QuadrotorSim/QuadrotorSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuadrotorSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUADROTORSIM_API AQuadrotorSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
